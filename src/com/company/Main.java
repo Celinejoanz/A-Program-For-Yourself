31259dbad46491b167fffc6efee959426ac25a75
Chidimma Uba <chidimmajuba@gmail.com>
2018-05-31 14:06:23 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This is my mini bio
+        System.out.println( "My name is Chidimma Uba");
+        System.out.println( "I am old enough");
+    }
+}
